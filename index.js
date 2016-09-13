bd14910ea82ef900bcfcf27d7e9ece3d7b0f9b4e
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-13 21:54:58 +0900

    keep retain

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-require('http').createServer((req, res) => res.end('hello leo nishizawa')
-).listen(process.env.PORT || 3000);
+void function () {
+	'use strict';
+
+	const http = require('http');
+	const url = require('url');
+	let counter = 0;
+
+	http.createServer((req, res) => {
+		res.end('hello leo nishizawa ' + (++counter));
+	}).listen(process.env.PORT || 3000);
+
+	(process.env.APP_URLS || '')
+		.split(';')
+		.filter(x => x)
+		.forEach(x => {
+			const opts = url.parse(x);
+			opts.method = 'GET';
+
+			http.request(opts, res => 0).end(); 
+			setInterval(() => {
+				http.request(opts, res => 0).end(); 
+			}, 15 * 60 * 1000)
+		});
+
+} ();
